8d24cf46225a51368211bf09e4d9ea9b08d65fc9
butterkitty <plarina@gmail.com>
2020-07-11 16:33:34 -0400

    Fix botched merge

--- packages/mediacenter/kodi-binary-addons/audiodecoder.ssf/package.mk
+++ packages/mediacenter/kodi-binary-addons/audiodecoder.ssf/package.mk
@@ -3,13 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="audiodecoder.ssf"
-<<<<<<< HEAD
-PKG_VERSION="2.2.2-Matrix"
-PKG_SHA256="f0b9a44453660f45e48f14bcb21d8d582bf5f96fc3b1622c421454525e64b112"
-=======
 PKG_VERSION="2.2.0-Matrix"
 PKG_SHA256="4f137040701318e420577f391e1ea3e75addfeba320706fcd525fd1b4f96b2ad"
->>>>>>> parent of 6dbbad0e3e... kodi-binary-addons: update to latest versions
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
