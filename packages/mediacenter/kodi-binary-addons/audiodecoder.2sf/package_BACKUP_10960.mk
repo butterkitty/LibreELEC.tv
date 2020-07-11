1d8d0f62c36bf6ee5f59bbb731427e5772300955
mplarina <mplarina@inspirehub.com>
2020-07-11 15:07:54 -0400

    Fix to allow Kodi and Python2 to build properly

---
+++ packages/mediacenter/kodi-binary-addons/audiodecoder.2sf/package_BACKUP_10960.mk
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="audiodecoder.2sf"
+<<<<<<< HEAD
+PKG_VERSION="2.1.4-Matrix"
+PKG_SHA256="b3cf672759f2cd1ed97795b5b85f12ae460b1d0bcbec8e3a6e51196a217af6f9"
+=======
+PKG_VERSION="2.1.2-Matrix"
+PKG_SHA256="42ffadc6c82ec0b6274508b29ad75bd2d5421044ebc3686d6466fe68808ddf9a"
+>>>>>>> parent of 6dbbad0e3e... kodi-binary-addons: update to latest versions
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/xbmc/audiodecoder.2sf"
+PKG_URL="https://github.com/xbmc/audiodecoder.2sf/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform"
+PKG_SECTION=""
+PKG_SHORTDESC="audiodecoder.2sf"
+PKG_LONGDESC="audiodecoder.2sf"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="kodi.audiodecoder"
