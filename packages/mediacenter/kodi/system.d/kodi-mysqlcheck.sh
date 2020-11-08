e02a36f7598a500c9fe3ec35eeac7db63f8922f6
butterkitty <micher@rescuityonline.com>
2020-11-08 16:16:17 -0500

    Fixed kodi-mysqlcheck.sh to use date for timing and curl

--- packages/mediacenter/kodi/system.d/kodi-mysqlcheck.sh
+++ packages/mediacenter/kodi/system.d/kodi-mysqlcheck.sh
@@ -1,8 +1,8 @@
-counter=0
+startTime=$(date +%s)
 advancedSettings="/storage/.kodi/userdata/advancedsettings.xml"
 
-if [ $1 -lt 5 ]; then # If the waitonnetwork time is less than 5, we may need more than that for the network to come up in full, so make it 5 in this context
-  maxCounter=5
+if [ $1 -lt 10 ]; then # If the waitonnetwork time is less than 5, we may need more than that for the network to come up in full, so make it 5 in this context
+  maxCounter=10
 else
   maxCounter=$1
 fi
@@ -15,8 +15,8 @@ if grep -qF "<!--<musicdatabase>" ${advancedSettings} && awk '/<musicdatabase>/{
 fi
 
 
-while ! /bin/ping -c1 google.com ; do # If there is no real internet connection, loop through. Checking using google.com as a very reliable source
-  if [ $counter -eq $maxCounter ]; then # If we've tried to ping for a real internet connection as many times as the network is set to wait
+while [[ $curlTest -ne 22 ]] && [[ $curlTest -ne 0 ]] ; do # If there is no real internet connection, loop through. Checking using google.com as a very reliable source
+  if [ $(($(date +%s) - $startTime)) -ge $maxCounter ]; then # If we've tried to ping for a real internet connection as many times as the network is set to wait
     if ! grep -qF "<!--<videodatabase>" ${advancedSettings} && awk '/<videodatabase>/{flag=1;next}/<\/videodatabase>/{flag=0}flag' ${advancedSettings} | grep -q "<type>mysql"; then # If the advancedsettings.xml is mysql enabled, disable it
       sed -i ':a;N;$!ba;s/<videodatabase>\(.*\)<\/videodatabase>/<!--&-->/g' ${advancedSettings}
     fi
@@ -25,7 +25,6 @@ while ! /bin/ping -c1 google.com ; do # If there is no real internet connection,
     fi
     exit 0
   fi
-  sleep 1
-  counter=$(( $counter+1 ))
+  curlTest=$(curl -sL --fail http://minecraft.inspirehub.com -o /dev/null; echo $?)
 done
 exit 0
