fa54f9a0ea39f5470e262ddb9558fb3e31e66873
butterkitty <micher@rescuityonline.com>
2020-11-10 08:18:42 -0500

    Updated mesa to 19.2.8 in order to support Panfrost

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="19.1.7"
-PKG_SHA256="97c9f6a6127bee5ab21c3fe63ff3e0bd73a7966415f92f66500b0b276b7150da"
+PKG_VERSION="19.2.8"
+PKG_SHA256="CFB0C7E71DA993505B11E28AF02B50172CA1F9CBCC321EE199812FF84C201710"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://github.com/mesa3d/mesa/archive/mesa-$PKG_VERSION.tar.gz"
